98fba8489a7a859b0939301be6927424626a7ac8
Stefan Adolf <stefan@welshare.health>
2025-05-14 11:50:37 +0200

    wc session display
    Signed-off-by: Stefan Adolf <stefan@welshare.health>

--- app/_/Wallet.tsx
+++ app/_/Wallet.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useWalletKit } from "@/components/context/WalletKitContext";
 import { WalletConnectIcon } from "@/components/icons/WalletConnectIcon";
 import { Button } from "@/components/ui/button";
 import {
@@ -12,6 +13,8 @@ import {
 import { WalletConnectDialog } from "@/components/wallet-connect-dialog";
 import truncateEthAddress from "@/lib/truncate";
 import { usePrivy } from "@privy-io/react-auth";
+import { getSdkError } from "@walletconnect/utils";
+import { useCallback } from "react";
 import { useAccount, useChainId, useDisconnect } from "wagmi";
 
 const ConnectButton = () => {
@@ -25,8 +28,20 @@ const ConnectedWallet = () => {
   const { address } = useAccount();
   const { disconnect } = useDisconnect();
   const chainId = useChainId();
+  const { sessions, walletKit } = useWalletKit();
 
   const { user, logout } = usePrivy();
+
+  const disconnectSession = useCallback(
+    async (topic: string) => {
+      if (!walletKit) return;
+      await walletKit.disconnectSession({
+        topic,
+        reason: getSdkError("USER_DISCONNECTED"),
+      });
+    },
+    [walletKit]
+  );
 
   const emailAddress = user?.google?.email || user?.email?.address;
   return (
@@ -44,7 +59,22 @@ const ConnectedWallet = () => {
           <DropdownMenuSeparator />
           <DropdownMenuLabel>{emailAddress}</DropdownMenuLabel>
           <DropdownMenuLabel>Chain {chainId}</DropdownMenuLabel>
-          {/* <WalletConnectSessions />  */}
+          {sessions &&
+            Object.values(sessions).map((session) => (
+              <DropdownMenuItem
+                key={session.topic}
+                onClick={() => disconnectSession(session.topic)}
+              >
+                <img
+                  src={session.peer.metadata.icons[0]}
+                  alt={session.peer.metadata.name}
+                  className="w-5 h-5 object-cover"
+                />
+                <span className="font-medium text-foreground">
+                  {session.peer.metadata.name}
+                </span>
+              </DropdownMenuItem>
+            ))}
           <DropdownMenuItem
             onClick={async () => {
               await logout();
